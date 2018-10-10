9e1ead0a866ffd75c81ebce1c7f51216ca2e273d
elina25 <elina_trips@hotmail.com>
2018-10-10 23:19:21 +0300

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -53,7 +53,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "dimagalishev@gmail.com";
+$address = "";
 
 
 // Configuration option.
